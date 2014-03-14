39c61060a557d1c80047197414d02c67c7416966
Wei Dong <wdong@wdong.org>
2014-03-14 12:22:28 -0400

    ..

--- make-release.sh
+++ make-release.sh
@@ -3,4 +3,12 @@
 vagrant up
 vagrant ssh -c /vagrant/make-release-vagrant.sh
 vagrant halt
+if [ ! -f kgraph-1.1-x86_64.tar.gz ]
+then
+    echo FAILED
+    exit
+fi
+
+scp kgraph-1.1-x86_64.tar.gz cloud:/var/www/kgraph/releases
+
 
