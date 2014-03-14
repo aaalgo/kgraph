df103d64fc6154a4f1ec090cfd296a161d4f36c1
Wei Dong <wdong@wdong.org>
2014-03-14 12:27:13 -0400

    ...

--- make-release-vagrant.sh
+++ make-release-vagrant.sh
@@ -5,11 +5,8 @@ git clone /vagrant
 cd vagrant
 make
 make release
-cp *.tar.gz /vagrant
 version=`cat version`
 release=kgraph-$version-x86_64
 mv kgraph-release $release
 tar zcvf $release.tar.gz $release
-
-
-
+cp $release.tar.gz /vagrant
